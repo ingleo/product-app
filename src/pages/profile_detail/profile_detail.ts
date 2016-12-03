bf2b75c791975b377a10ee934e77309484a06cba
elrichi2001 <elrichi2001>
2016-12-03 12:58:41 -0500

    Update

--- src/pages/profile_detail/profile_detail.ts
+++ src/pages/profile_detail/profile_detail.ts
@@ -1,38 +1,31 @@
 import { Component } from '@angular/core';
-
 import { NavController, NavParams } from 'ionic-angular';
+import { User } from '../../model/user'
+import { UserService } from '../../providers/user-service'
 
 @Component({
   selector: 'page-page2',
   templateUrl: 'profile_detail.html'
 })
-export class Page2 {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+export class profile_detail {
+  users: User[];
+  
+  constructor(public navCtrl: NavController, private userService: UserService) {}
+  
+  getUsers(){
+    this.userService.getUsers()
+      .subscribe(
+        users => {
+          this.users = users;
+        },
+        error => {
+          console.log(error);
+        });
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+  ngOnInit():void{
+      this.getUsers();
     }
-  }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Page2, {
-      item: item
-    });
-  }
+  
 }
