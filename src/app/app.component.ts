ac9821d43b39536ecf2d26166ad76fce3fad908f
gjrivero <gjrivero@gmail.com>
2016-12-03 12:01:32 -0500

    summit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { Pagelogin } from '../pages/pagelogin/pagelogin';
 
 
 @Component({
@@ -22,7 +22,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Page login', component: Pagelogin }
     ];
 
   }
