0e8f010303df3c3f9d08fcdec015a87343ddc288
Leonardo González <clgrhc@gmail.com>
2016-12-10 01:12:57 -0500

    fab y refresher

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Product } from '../../model/product';
-import {ProductService} from "../../providers/product.service";
+import { ProductService } from "../../providers/product.service";
 import { ProductDetailPage } from '../product-detail/product-detail';
+import { CreateProductPage } from '../create-product/create-product';
 
 @Component({
   selector: 'page-page2',
@@ -11,30 +12,42 @@ import { ProductDetailPage } from '../product-detail/product-detail';
 export class Home {
 
   products: Product[];
+  createProductPage = CreateProductPage;
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private productService: ProductService) {
+    public navParams: NavParams,
+    private productService: ProductService) {
 
+  }
+
+  ionViewWillEnter() {
     this.getProducts();
-
   }
 
   getProducts() {
     this.productService.getProducts()
       .subscribe(
-        products => {
-          this.products = products;
-        },
+      products => {
+        this.products = products;
+      },
 
-        error => {
-          console.log(error);
-        }
+      error => {
+        console.log(error);
+      }
       );
   }
 
-  onSelect(id: number){
-    this.navCtrl.push(ProductDetailPage,{p:id});
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+    setTimeout(() => {
+      this.getProducts();
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+  onSelect(id: number) {
+    this.navCtrl.push(ProductDetailPage, { p: id });
   }
 
 
