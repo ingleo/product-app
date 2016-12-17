69f22192f2a16570cd197d3deeffa0320dce98de
Leonardo González <clgrhc@gmail.com>
2016-12-17 09:56:02 -0500

    se agrega icon y splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,6 +37,8 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      //habilitar icono
+      Splashscreen.show();
       Splashscreen.hide();
     });
   }
